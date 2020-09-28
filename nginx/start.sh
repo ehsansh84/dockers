d63b2e9a7824b933be55ffeb61c953605da44f86
ehsan <ehsan@dockers-68.faraso.org>
2020-09-28 06:08:26 -0400

    jenkins added

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,3 +1,5 @@
-sudo docker-compose -f nginx.yml down 
-sudo docker-compose -f nginx.yml up -d
-sudo docker exec ng service php7.3-fpm restart
+docker-compose -f nginx.yml down 
+docker-compose -f nginx.yml up -d
+docker exec ng service php7.3-fpm restart
+docker network connect dockers_default ng
+
