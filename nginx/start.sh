01bd336adb6f14a4b3170a66eb53156d0b9b3c5a
ehsan <ehsan.shirzadi@gmail.com>
2021-04-14 14:58:47 +0430

    rabbitmq added

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,4 +1,4 @@
-docker-compose -f nginx.yml down 
+#docker-compose -f nginx.yml down 
 docker-compose -f nginx.yml up -d
 docker exec ng service php7.3-fpm restart
 docker exec ng apt-get install -y python-certbot-nginx
